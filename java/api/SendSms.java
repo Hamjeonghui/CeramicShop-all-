8b0a71e9bc659a8103aa6926db8f76c4a3317f7a
Hamjeonghui <100455658+Hamjeonghui@users.noreply.github.com>
2022-04-02 09:28:00 +0900

    Add files via upload

---
+++ java/api/SendSms.java
@@ -0,0 +1,35 @@
+package api;
+
+import java.util.HashMap;
+
+import org.json.simple.JSONObject;
+
+import net.nurigo.java_sdk.api.Message;
+import net.nurigo.java_sdk.exceptions.CoolsmsException;
+
+public class SendSms {
+
+//사용 한도 14번 ^^
+
+	static public void sendSms(String to, String msg) {
+		String api_key = "API key";
+		String api_secret = "secret key";
+		Message coolsms = new Message(api_key, api_secret);
+
+		// 4 params(to, from, type, text) are mandatory. must be filled
+		HashMap<String, String> params = new HashMap<String, String>();
+		params.put("to", to); // 수신자
+		params.put("from", "발신자 번호(01000000000)"); // 발신자
+		params.put("type", "SMS");
+		params.put("text", "필기 아이디 : " + msg); // 문자내용
+		params.put("app_version", "test app 1.2"); // application name and version
+
+		try {
+			JSONObject obj = (JSONObject) coolsms.send(params);
+			System.out.println(obj.toString());
+		} catch (CoolsmsException e) {
+			System.out.println(e.getMessage());
+			System.out.println(e.getCode());
+		}
+	}
+}
